bea67442e077561afa2895fe2934bf6dcbc4cad7
Sanghyun Lee <hammerha@gmail.com>
2020-07-05 08:06:32 +0200

    Tweak code - do

--- ricotta/src/ricotta/core.clj
+++ ricotta/src/ricotta/core.clj
@@ -7,12 +7,11 @@
   (if-not (empty? args)
     (let [env (nth args 0)
           config (config env)]
-      (do
-        (transform-data
-          (:input-csv-path config)
-          (:output-json-path config))
-        (generate-ui
-          (:js-template-path config)
-          (:output-json-path config)
-          (:js-output-path config))))
+      (transform-data
+        (:input-csv-path config)
+        (:output-json-path config))
+      (generate-ui
+        (:js-template-path config)
+        (:output-json-path config)
+        (:js-output-path config)))
     (throw (Exception. "Env is needed - local or prod?"))))
