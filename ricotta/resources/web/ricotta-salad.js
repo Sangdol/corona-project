8ffc0c270f5d8b12085fa604232f7b01e54404e6
Sanghyun Lee <hammerha@gmail.com>
2020-07-05 10:59:33 +0200

    Adjust column for mobile

--- ricotta/resources/web/ricotta-salad.js
+++ ricotta/resources/web/ricotta-salad.js
@@ -1,15 +1,35 @@
+// https://stackoverflow.com/a/10601315/524588
+function abbreviateNumber(value) {
+    var newValue = value;
+    if (value >= 1000) {
+        var suffixes = ["", "K", "M", "B", "T"];
+        var suffixNum = Math.floor( (""+value).length/3 );
+        var shortValue = '';
+        for (var precision = 2; precision >= 1; precision--) {
+            shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
+            var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+            if (dotLessShortValue.length <= 2) { break; }
+        }
+        if (shortValue % 1 != 0)  shortValue = shortValue.toFixed(1);
+        newValue = shortValue+suffixes[suffixNum];
+    }
+    return newValue;
+}
+
+tabledata.forEach((country) => {
+  country.populationShort = abbreviateNumber(country.population);
+});
+
 const table = new Tabulator("#container", {
-    height: 500, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
-    data: tabledata, //assign data to table
-    layout: "fitColumns", //fit columns to width of table (optional)
-    columns: [ //Define Table Columns
-        {title: "Country", field: "location", width:150},
-        //{title: "Age", field: "age", hozAlign: "left", formatter: "progress"},
-        {title: "Date", field: "date"},
-        {title: "Total Cases", field: "total_cases"},
-        {title: "New Cases", field: "new_cases"},
-        {title: "Total Cases per Milion", field: "total_cases_per_million"},
-        {title: "New Cases per Milion", field: "new_cases_per_million"},
-        {title: "Population", field: "population"},
+    data: tabledata,
+    layout: "fitColumns",
+    columns: [
+        {title: "Country", field: "location"},
+        // {title: "Date", field: "date"},
+        {title: "Total", field: "total_cases"},
+        {title: "New", field: "new_cases"},
+        // {title: "Total/M", field: "total_cases_per_million"},
+        // {title: "New/M", field: "new_cases_per_million"},
+        {title: "POP", field: "populationShort"},
     ],
 });
