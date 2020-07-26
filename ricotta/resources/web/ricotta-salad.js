b78afd5d0651d211e923579db8bb16339be6f532
Sanghyun Lee <hammerha@gmail.com>
2020-07-26 22:52:56 +0200

    Add per million data

--- ricotta/resources/web/ricotta-salad.js
+++ ricotta/resources/web/ricotta-salad.js
@@ -1,35 +1,74 @@
 // https://stackoverflow.com/a/2901298/524588
-function numberWithCommasFormatter(cell, formatterParams, onRendered) {
-    return cell.getValue().toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+function addCommas(n) {
+  return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
+
+// https://stackoverflow.com/a/19722641/524588
+Number.prototype.round = function(places) {
+  return +(Math.round(this + "e+" + places)  + "e-" + places);
 }
 
 tabledata.countries.forEach((country) => {
   if (country.date == tabledata.date) {
     country.name_and_date = country.location;
   } else {
-    country.name_and_date = `${country.location}<br><span class="sub-text">${formatDateShort(country.date)}</span>`;
+    country.name_and_date =
+      `${country.location}<br>
+      <span class="sub-text">${formatDateShort(country.date)}</span>`;
   }
+
+  country.new_cases_and_per_million =
+    `${addCommas(country.new_cases)}<br>
+    <span class="sub-text">${addCommas(country.new_cases_per_million.round(2))} per million</span>`;
+
+  country.total_cases_and_per_million =
+    `${addCommas(country.total_cases)}<br>
+    <span class="sub-text">${addCommas(country.total_cases_per_million.round(0))} per million</span>`;
 });
+
+// http://tabulator.info/docs/4.7/sort#func-custom
+// takes number from "number + sub-text" html
+function multilineSorter(a, b) {
+    function cases(text) {
+      return text.match(/[\d,]+/)[0].replace(/,/g, "");
+    }
+
+    return cases(a) - cases(b);
+}
 
 const table = new Tabulator("#table", {
     selectable: false,
     data: tabledata.countries,
     layout: "fitColumns",
     initialSort:[
-        {column:"total_cases", dir:"desc"},
-        {column:"new_cases", dir:"desc"},
+        {column:"total_cases_and_per_million", dir:"desc"},
+        {column:"new_cases_and_per_million", dir:"desc"},
     ],
     columns: [
-        {title: "Country", field: "name_and_date", widthGrow: 6, formatter: "html", resizable: false},
+        {
+          title: "Country",
+          field: "name_and_date",
+          widthGrow: 6,
+          formatter: "html",
+          resizable: false
+        },
         {
           title: `New<br><span class="sub-text">Daily</span>`,
           titleFormatter: "html",
-          field: "new_cases",
+          field: "new_cases_and_per_million",
           widthGrow: 4,
-          formatter: numberWithCommasFormatter,
+          formatter: "html",
+          sorter: multilineSorter,
           resizable: false
         },
-        {title: "Total", field: "total_cases", widthGrow: 5, formatter: numberWithCommasFormatter, resizable: false},
+        {
+          title: "Total",
+          field: "total_cases_and_per_million",
+          widthGrow: 5,
+          formatter: "html",
+          sorter: multilineSorter,
+          resizable: false
+        },
     ],
 });
 
